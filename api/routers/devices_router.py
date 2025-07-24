5938afa499845b04a80adc85dbb9866906a594ea
Anastasia Shachenkova <nastya.nba888@gmail.com>
2025-07-24 10:09:09 +0300

    fix devices_router.py

--- api/routers/devices_router.py
+++ api/routers/devices_router.py
@@ -2,6 +2,7 @@ from uuid import UUID
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from api.schemas.devices_schema import DeviceSchemaGet, DeviceSchemaPost, DeviceSchemaUpdate
+from autorization.auth import get_current_user
 from database.crud.devices_crud import get_all_devices, get_device_by_id, create_device, update_device, delete_device
 from database.db import get_db
 from database.models import Devices
@@ -10,7 +11,8 @@ device_router = APIRouter(prefix='/device', tags=['Устройства'])
 
 
 @device_router.get('/all_devices')
-def all_devices(db: Session = Depends(get_db)):
+def all_devices(db: Session = Depends(get_db),
+                user: dict = Depends(get_current_user)):
     try:
         orm_models = get_all_devices(db)
         result = [DeviceSchemaGet.model_validate(m) for m in orm_models]
@@ -20,7 +22,9 @@ def all_devices(db: Session = Depends(get_db)):
 
 
 @device_router.get('/get_device/{device_id}')
-def device_by_id(device_id: UUID, db: Session = Depends(get_db)):
+def device_by_id(device_id: UUID,
+                 db: Session = Depends(get_db),
+                 user: dict = Depends(get_current_user)):
     try:
         orm_model = get_device_by_id(db, device_id)
         result = DeviceSchemaGet.model_validate(orm_model)
@@ -30,7 +34,9 @@ def device_by_id(device_id: UUID, db: Session = Depends(get_db)):
 
 
 @device_router.post('/create_device')
-def add_device(device: DeviceSchemaPost, db: Session = Depends(get_db)):
+def add_device(device: DeviceSchemaPost,
+               db: Session = Depends(get_db),
+               user: dict = Depends(get_current_user)):
     try:
         db_device = Devices(model_id=device.model_id, filial_id=device.filial_id, serial_number=device.serial_number)
         result = create_device(db, db_device)
@@ -40,7 +46,10 @@ def add_device(device: DeviceSchemaPost, db: Session = Depends(get_db)):
 
 
 @device_router.patch('/update_device/{device_id}')
-def update_device_by_id(device_id: UUID, device: DeviceSchemaUpdate, db: Session = Depends(get_db)):
+def update_device_by_id(device_id: UUID,
+                        device: DeviceSchemaUpdate,
+                        db: Session = Depends(get_db),
+                        user: dict = Depends(get_current_user)):
     try:
         data_to_update = device.model_dump(exclude_unset=True)
         return update_device(db, device_id, changes=data_to_update)
@@ -49,7 +58,9 @@ def update_device_by_id(device_id: UUID, device: DeviceSchemaUpdate, db: Session
 
 
 @device_router.delete('/delete_device/{device_id}')
-def delete_device_by_id(device_id: UUID, db: Session = Depends(get_db)):
+def delete_device_by_id(device_id: UUID,
+                        db: Session = Depends(get_db),
+                        user: dict = Depends(get_current_user)):
     try:
         return delete_device(db, device_id)
     except Exception as e:
