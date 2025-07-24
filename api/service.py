63455baa657bdacd3438c89200882ea858693a48
Anastasia Shachenkova <nastya.nba888@gmail.com>
2025-07-24 10:10:50 +0300

    fix file service.py

--- api/service.py
+++ api/service.py
@@ -3,8 +3,9 @@ from api.routers.devices_router import device_router
 from api.routers.enterprises_router import enterprise_router
 from api.routers.filial_enterprises_router import filial_enterprise_router
 from api.routers.device_models_router import device_model_router
-from api.routers.regular_rimes_router import regular_time_router
+from api.routers.regular_times_router import regular_time_router
 from api.routers.task_lists_router import task_list_router
+from autorization.auth import auth_router
 from fastapi import FastAPI
 
 
@@ -16,3 +17,4 @@ app.include_router(filial_enterprise_router)
 app.include_router(task_list_router)
 app.include_router(regular_time_router)
 app.include_router(config_router)
+app.include_router(auth_router)
